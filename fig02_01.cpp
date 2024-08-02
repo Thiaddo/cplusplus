6068d2c762f56b9ae3c4dc5c6a6dbb76e948fe89
Thiago Melo <thiado@live.com>
2024-08-02 09:53:53 -0300

    Livro C++

---
+++ fig02_01.cpp
@@ -0,0 +1,11 @@
+// Figura 2.1: fig02_01.cpp
+// Programa de impressão de texto.
+#include <iostream> // permite que o programa gere saída de dados na tela
+
+// a função inicia com a execução do programa
+int main()
+{
+    std::cout << "Welcome";   // exibe a mensagem
+    std::cout << "to C++!\n";
+    return 0; // indica que o programa terminou com sucesso
+} // fim da função
